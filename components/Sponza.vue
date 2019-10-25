37917da9081b78b2205140a6ad66db4529d154c6
Nicholas <zxzflow@gmail.com>
2019-10-24 17:18:01 -0700

    Added sponsor section

--- components/Sponza.vue
+++ components/Sponza.vue
@@ -1,20 +1,90 @@
 <template>
   <div class="sponza">
-    <h2>Sponsors</h2>
-    <!-- <div id="sponsors-list" :class="'is-flex'">
-      <div v-for="item in items" :key="item.name">
-        <a :href="item.url" target="_blank">
-          <img :src="item.imageURL" :alt="item.name">
-        </a>
+    <h2 style="margin-bottom: 30px;">
+      Sponsors
+    </h2>
+    <br>
+    <!--  -->
+    <div id="sponsors-list" :class="is-flex">
+      <div class="columns is-multiline is-vcentered is-centered">
+        <div v-for="item in items" :key="item.name">
+          <div v-if="item.rank == 'tera'">
+            <div class="column">
+              <a :href="formatURL(item.url)" target="_blank">
+                <img class="tera" :src="item.imageURL" :alt="item.name">
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </div> -->
-    <Button title="Become a Sponsor" url="mailto:lhd@nwplus.io" is-wide="true" />
+    </div>
+    <!--  -->
+    <!--  -->
+    <div id="sponsors-list" :class="is-flex">
+      <div class="columns is-multiline is-vcentered is-centered">
+        <div v-for="item in items" :key="item.name">
+          <div v-if="item.rank == 'giga'">
+            <div class="column">
+              <a :href="formatURL(item.url)" target="_blank">
+                <img class="giga" :src="item.imageURL" :alt="item.name">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--  -->
+    <!--  -->
+    <div id="sponsors-list" :class="is-flex">
+      <div class="columns is-multiline is-vcentered is-centered">
+        <div v-for="item in items" :key="item.name">
+          <div v-if="item.rank == 'mega'">
+            <div class="column">
+              <a :href="formatURL(item.url)" target="_blank">
+                <img class="mega" :src="item.imageURL" :alt="item.name">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--  -->
+    <!--  -->
+    <div id="sponsors-list" :class="is-flex">
+      <div class="columns is-multiline is-vcentered is-centered">
+        <div v-for="item in items" :key="item.name">
+          <div v-if="item.rank == 'kilo'">
+            <div class="column">
+              <a :href="formatURL(item.url)" target="_blank">
+                <img class="kilo" :src="item.imageURL" :alt="item.name">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--  -->
+    <!--  -->
+    <div id="sponsors-list" :class="is-flex">
+      <div class="columns is-multiline is-vcentered is-centered">
+        <div v-for="item in items" :key="item.name">
+          <div v-if="item.rank == 'in-kind'">
+            <div class="column">
+              <a :href="formatURL(item.url)" target="_blank">
+                <img class="in-kind" :src="item.imageURL" :alt="item.name">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--  -->
+    <Button style="margin-top: 50px;" title="Become a Sponsor" url="mailto:logistics@nwplus.io" is-wide="true" />
   </div>
 </template>
 
 <script>
 import Button from '~/components/Button.vue'
-
 export default {
   components: {
     Button
@@ -24,6 +94,15 @@ export default {
       type: Array,
       required: true
     }
+  },
+  methods: {
+    formatURL(url) {
+      if (!url.includes('http')) {
+        return `http://${url}`
+      } else {
+        return url
+      }
+    }
   }
 }
 </script>
@@ -31,8 +110,25 @@ export default {
 <style scoped lang="scss">
 @import "bulma/bulma.sass";
 $heading-font: "Caveat Brush";
-
 //Desktop CSS:
+.columns {
+  margin-bottom: 20px !important;
+}
+.tera {
+  width: 400px;
+}
+.giga {
+  max-width: 350px;
+}
+.mega {
+  max-width: 300px;
+}
+.kilo {
+  max-width: 225px;
+}
+.in-kind {
+  max-width: 150px;
+}
 h2 {
   font-family: $heading-font;
   font-size: 48px;
@@ -42,9 +138,7 @@ h2 {
   margin-top: 5%;
   text-align: center;
 }
-
 //Mobile CSS:
 @include until ($desktop) {
-
   }
 </style>
