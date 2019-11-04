ab90aa1bb938c28d8584aa480f7e941d8dab287d
Austin Kobayashi <austinkobayashi@gmail.com>
2019-11-04 13:37:22 -0800

    linkedin opens in new tab

--- components/TeamSection.vue
+++ components/TeamSection.vue
@@ -4,7 +4,7 @@
       <li v-for="index in 112" :key="index" class="container">
         <img :src="'/team-section/Vector' + index + '.svg'" class="teamPic">
         <div v-if="imageIndexes.includes(index) && linkedins[index] !== ''">
-          <a :href="linkedins[index]" class="icon" title="LinkedIn">
+          <a :href="linkedins[index]" class="icon" title="LinkedIn" target="_blank">
             <img class="linkedin" src="/team-section/linkedin.svg">
           </a>
         </div>
