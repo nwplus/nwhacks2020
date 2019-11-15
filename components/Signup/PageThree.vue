daf51efe742af8dc27e97a84ff8fc4c354239685
Luce <lucy_liu@live.ca>
2019-11-14 22:28:01 -0800

    continue styles

--- components/Signup/PageThree.vue
+++ components/Signup/PageThree.vue
@@ -1,99 +1,100 @@
 <template>
   <section>
-    <!-- <div class="columns"> -->
-    <!-- <div class="column pgthree"> -->
-    <b-field
-      label="How did you hear about nwHacks?"
-      class="required"
-      :type="v.hacker.source.$error ? 'is-danger' : ''"
-      :message="v.hacker.source.$error ? (!v.hacker.source.required ? 'Required' : '') : ''"
-    >
-      <b-select v-model.trim="v.hacker.source.$model" placeholder="Select" required>
-        <option value="MLH">
-          MLH
-        </option>
-        <option value="Social media">
-          Social media
-        </option>
-        <option value="Website">
-          Website
-        </option>
-        <option value="Word of mouth">
-          Word of mouth
-        </option>
-        <option value="Club newsletter">
-          Club newsletter
-        </option>
-        <option value="Faculty newsletter">
-          Faculty newsletter
-        </option>
-        <option value="Prof-inclass">
-          Prof/in class
-        </option>
-        <option value="Other">
-          Other
-        </option>
-      </b-select>
-    </b-field>
-    <b-field
-      v-if="v.hacker.source.$model === 'Other'"
-      label="Please specify where you heard about nwHacks"
-      class="required"
-      :type="v.hacker.sourceother.$error ? 'is-danger' : ''"
-      :message="v.hacker.sourceother.$error ? (!v.hacker.sourceother.required ? 'Required' : '') : ''"
-    >
-      <b-input v-model.trim="v.hacker.sourceother.$model" />
-    </b-field>
-    <p>
-      🤖 We participate in Major League Hacking (MLH) as a MLH Member Event and are committed to providing a safe and inclusive environment for everyone attending nwHacks as required by the
-      <a
-        href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
-      >MLH Code of Conduct</a>.
-    </p>
-    <div class="field">
-      <b-checkbox v-model.trim="v.hacker.isPrivacyPolicyChecked.$model">
-        I authorize nwPlus to share certain application/registration information for event administration, ranking, MLH administration, and occasional messages about hackathons in line with the
-        <a
-          href="https://mlh.io/privacy"
-        >MLH Privacy Policy</a>.
-      </b-checkbox>
-    </div>
-    <div class="field">
-      <b-checkbox v-model.trim="v.hacker.isCodeOfConductChecked.$model">
-        I have read and agree to the
-        <a
-          href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
-        >Major League Hacking Code of Conduct</a>.
-      </b-checkbox>
-    </div>
-    <div class="field">
-      <p>💾 We use your (anonymized!) data to help you get the best sponsors and continuously improve nwHacks with each iteration.</p>
-      <b-checkbox
-        v-model.trim="v.hacker.isDataReportingChecked.$model"
-      >
-        I authorize nwPlus to use my anonymized data for data reporting.
-      </b-checkbox>
-    </div>
-    <div class="field">
-      <p>💼 Our hackathon aims to connect you with industry professionals, recruiters, and career opportunities. In doing so, information about our hackers is needed in order for attending companies to contact you.</p>
-      <b-checkbox>I authorize nwPlus to provide my resume and supporting documents (Github, Linkedin, etc) to event sponsors for recruitment purposes upon request.</b-checkbox>
-    </div>
-    <div class="field">
-      <p>Connect with the community of nwHacks on Medium, Twitter, and Facebook! Share your story and excitement with us!</p>
-      <div class="icons">
-        <a href="https://medium.com/nwhacks">
-          <font-awesome-icon :icon="['fab', 'medium-m']" />
-        </a>
-        <a href="https://twitter.com/nwhacks">
-          <font-awesome-icon :icon="['fab', 'twitter']" />
-        </a>
-        <a href="https://www.facebook.com/nwHacks/">
-          <font-awesome-icon :icon="['fab', 'facebook-f']" />
-        </a>
+    <div class="columns">
+      <div id="page-three" class="column">
+        <b-field
+          label="How did you hear about nwHacks?"
+          custom-class="is-small"
+          class="required"
+          :type="v.hacker.source.$error ? 'is-danger' : ''"
+          :message="v.hacker.source.$error ? (!v.hacker.source.required ? 'Required' : '') : ''"
+        >
+          <b-select v-model.trim="v.hacker.source.$model" placeholder="Select" required>
+            <option value="MLH">
+              MLH
+            </option>
+            <option value="Social media">
+              Social media
+            </option>
+            <option value="Website">
+              Website
+            </option>
+            <option value="Word of mouth">
+              Word of mouth
+            </option>
+            <option value="Club newsletter">
+              Club newsletter
+            </option>
+            <option value="Faculty newsletter">
+              Faculty newsletter
+            </option>
+            <option value="Prof-inclass">
+              Prof/in class
+            </option>
+            <option value="Other">
+              Other
+            </option>
+          </b-select>
+        </b-field>
+        <b-field
+          v-if="v.hacker.source.$model === 'Other'"
+          label="Please specify where you heard about nwHacks"
+          class="required"
+          :type="v.hacker.sourceother.$error ? 'is-danger' : ''"
+          :message="v.hacker.sourceother.$error ? (!v.hacker.sourceother.required ? 'Required' : '') : ''"
+        >
+          <b-input v-model.trim="v.hacker.sourceother.$model" />
+        </b-field>
+        <p>
+          🤖 We participate in Major League Hacking (MLH) as a MLH Member Event and are committed to providing a safe and inclusive environment for everyone attending nwHacks as required by the
+          <a
+            href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
+          >MLH Code of Conduct</a>.
+        </p>
+        <div class="field">
+          <b-checkbox v-model.trim="v.hacker.isPrivacyPolicyChecked.$model">
+            I authorize nwPlus to share certain application/registration information for event administration, ranking, MLH administration, and occasional messages about hackathons in line with the
+            <a
+              href="https://mlh.io/privacy"
+            >MLH Privacy Policy</a>.
+          </b-checkbox>
+        </div>
+        <div class="field">
+          <b-checkbox v-model.trim="v.hacker.isCodeOfConductChecked.$model">
+            I have read and agree to the
+            <a
+              href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
+            >Major League Hacking Code of Conduct</a>.
+          </b-checkbox>
+        </div>
+        <div class="field">
+          <p>💾 We use your (anonymized!) data to help you get the best sponsors and continuously improve nwHacks with each iteration.</p>
+          <b-checkbox
+            v-model.trim="v.hacker.isDataReportingChecked.$model"
+          >
+            I authorize nwPlus to use my anonymized data for data reporting.
+          </b-checkbox>
+        </div>
+        <div class="field">
+          <p>💼 Our hackathon aims to connect you with industry professionals, recruiters, and career opportunities. In doing so, information about our hackers is needed in order for attending companies to contact you.</p>
+          <b-checkbox>I authorize nwPlus to provide my resume and supporting documents (Github, Linkedin, etc) to event sponsors for recruitment purposes upon request.</b-checkbox>
+        </div>
+        <div class="field">
+          <p>Connect with the community of nwHacks on Medium, Twitter, and Facebook! Share your story and excitement with us!</p>
+          <div class="icons">
+            <a href="https://medium.com/nwhacks">
+              <font-awesome-icon :icon="['fab', 'medium-m']" />
+            </a>
+            <a href="https://twitter.com/nwhacks">
+              <font-awesome-icon :icon="['fab', 'twitter']" />
+            </a>
+            <a href="https://www.facebook.com/nwHacks/">
+              <font-awesome-icon :icon="['fab', 'facebook-f']" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
-    <!-- </div> -->
-    <!-- </div> -->
   </section>
 </template>
 
@@ -109,15 +110,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "bulma/bulma.sass";
+// @import "bulma/bulma.sass";
 $background-color: #f2f7fe;
 $font-family: "Apercu Pro", sans-serif;
-$font-colour: #21258a;
+$font-color: #21258a;
+$primary: $font-color;
+@import "~bulma/sass/utilities/_all";
 
-// .pgthree {
-//   margin: 30px 16px;
-//   @include from($tablet) {
-//     margin: 100px 380px;
-//   }
-// }
+section {
+  background-color: $background-color;
+}
+#page-three {
+  color: $font-color;
+  background-color: $background-color;
+  font-family: $font-family;
+  margin: 30px 16px;
+  @include from($tablet) {
+    margin: 100px 380px;
+  }
+}
+
+#page-three .label.question {
+  border: 2px solid blue;
+  color: $font-color;
+}
 </style>
