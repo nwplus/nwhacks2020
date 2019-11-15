e7e930a94d60a17f4116b040e08ca32ef5fbe0b0
Giulio Rossi <ciuffi9@mac.com>
2019-11-14 23:35:34 -0800

    style previous/next/submit buttons

--- pages/signUp.vue
+++ pages/signUp.vue
@@ -7,21 +7,16 @@
       <pageOne v-if="page == 0" :v="$v" />
       <pageTwo v-if="page == 1" :v="$v" />
       <pageThree v-if="page == 2" :v="$v" />
-      <section>
-        <b-button @click="submit">
-          Submit
-        </b-button>
-        <b-button @click="$store.commit('clearState')">
-          clear
-        </b-button>
-      </section>
-      <section>
-        <b-button @click="page--">
+      <section class="buttons">
+        <button class="button nav-button" @click="page--">
           Previous
-        </b-button>
-        <b-button @click="page++">
+        </button>
+        <button v-if="page !== 2" class="button nav-button" @click="page++">
           Next
-        </b-button>
+        </button>
+        <button v-if="page === 2" class="button submit-button" @click="submit">
+          Submit
+        </button>
       </section>
     </div>
     <signUpClosed v-if="page === -1" />
@@ -82,3 +77,27 @@ export default {
   }
 }
 </script>
+<style scoped>
+.nav-button {
+  background: linear-gradient(180deg, #FFFFFF 33.85%, #F1F8FB 66.15%, #E9F5F9 99.97%, #DBEFF5 99.98%, #B6E0EC 99.99%, #CEECF4 100%);
+  mix-blend-mode: normal;
+  /* primaryshade */
+  border: 1px solid #4483BD;
+  box-sizing: border-box;
+  border-radius: 4px;
+  box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.4);
+  color: #4483BD;
+  width: 100px;
+}
+.submit-button{
+  background: linear-gradient(180deg, #70B8CD 0%, #6572B0 76.56%, #6C62AF 99.99%, #693E94 100%);
+  border-radius: 4px;
+  box-shadow: 0px 0px 0px 0.2px #888888;
+  color: white;
+  width: 100px;
+}
+.buttons {
+  margin: 5% 0;
+  margin-left: 30%;
+}
+</style>
